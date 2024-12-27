d51726c4260597c7e7e56c37eabc99c20aee9e8e
Amy <amyskippy@outlook.com>
2024-12-27 23:55:14 +0000

    test: add DatabaseContext via Func to the test project

---
+++ test/EntityFrameworkCore.DataEncryption.Test/Context/DatabaseContextFunc.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace SoftFluent.EntityFrameworkCore.DataEncryption.Test.Context;
+
+public class DatabaseContextFunc : DbContext
+{
+    private readonly Func<IEncryptionProvider> _encryptionProvider;
+
+    public DbSet<AuthorEntity> Authors { get; set; }
+
+    public DbSet<BookEntity> Books { get; set; }
+
+    public DatabaseContextFunc(DbContextOptions options)
+        : base(options)
+    { }
+
+    public DatabaseContextFunc(DbContextOptions options, Func<IEncryptionProvider> encryptionProviderFunc = null)
+        : base(options)
+    {
+        _encryptionProvider = encryptionProviderFunc;
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.UseEncryption(_encryptionProvider);
+    }
+}
